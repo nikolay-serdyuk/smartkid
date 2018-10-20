cd475bb7e111801e3335c6e9947fddb6d4032ff0
Nikolay Serdyuk <nsr3333@mail.ru>
2018-10-20 20:46:15 +0700

    Added activity for evaluating expressions with parentheses (easy level)

--- app/src/main/java/com/nserdyuk/smartkid/GradeActivity.java
+++ app/src/main/java/com/nserdyuk/smartkid/GradeActivity.java
@@ -247,6 +247,11 @@ public class GradeActivity extends AppCompatActivity {
         intent.putExtra(Constants.ATTRIBUTE_FILE, "cm_kg_l.txt");
         nameToIntentMap.put(getResources().getString(R.string.activity_grade_cm_kg_l), intent);
 
+        intent = new Intent(this, ChatActivity.class);
+        intent.putExtra(Constants.ATTRIBUTE_EXAMPLES, 1);
+        intent.putExtra(Constants.ATTRIBUTE_FILE, "parens_easy.txt");
+        nameToIntentMap.put(getResources().getString(R.string.activity_grade_parens_easy), intent);
+
         // TODO: add files all_english_Masha23.txt
     }
 }
